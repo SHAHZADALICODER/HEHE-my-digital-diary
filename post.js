df990faa924ee7c2c289e2adff04b1f5d584b8d7
Shahzad Ali <shahgaming360@gmail.com>
2025-06-06 11:46:35 +0530

    add new post

--- post.js
+++ post.js
@@ -12,8 +12,28 @@ This blog is not just for others — it’s also for my future self. A place to 
 Welcome to my mind. Welcome to HEHEHE: My digital diary. (System detected : Need to be updated)`,
     },
 
-    '2': {title: 'Testing title', content: 'This is a test post content. It is just a placeholder to demonstrate the post structure.'},
+    '2': {title: 'Why I started this blog?',
+        content: `I started this blog to have a space where I can express myself freely. In a world where social media often feels like a performance, I wanted a place that’s just mine. A place to share my thoughts, ideas, and experiences without the pressure of likes or followers.
+
+        This is my own digital diary, where I can write about anything that comes to mind. Whether it’s about coding, game design, personal growth, or just random thoughts, this blog is my sanctuary. It’s a space where I can be myself, reflect on my journey, and connect with others who might feel the same way.
+        I also wanted to document my growth and changes over time. As I navigate through life, I find that writing helps me process my thoughts and emotions. It’s a way to understand myself better and to keep track of my journey.
+        I believe that writing helps me understand myself better. It’s a way to process my thoughts and emotions, and to document my growth. This blog is not just for me, but also for anyone who might find comfort or inspiration in my words. I hope that by sharing my experiences, I can connect with others who are on similar journeys.`,},
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Get post id from URL
